43f6f92110ed39bae0400d0947f54b7ee886266f
Ryan Hartley <ryan@elleestphotography.com>
2021-12-29 14:56:08 -0500

    orange btn border

--- components/ButtonBlue.js
+++ components/ButtonBlue.js
@@ -3,7 +3,7 @@
 
 export default function ButtonOrange({children}){
     return(
-        <button className="bg-btn-blue-bg  text-btn-blue-txt rounded-md p-3 px-5 uppercase font-bold text-sm tracking-widest hover:shadow-xl">{children}</button>
+        <button className="bg-btn-blue-bg border-none text-btn-blue-txt rounded-md p-3 px-5 uppercase font-bold text-sm tracking-widest hover:shadow-xl">{children}</button>
     )
 }
 
